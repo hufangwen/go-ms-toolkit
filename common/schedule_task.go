73f6927ac75ced129c196d933a901e9dc59f5018
leeningli <1507045477@qq.com>
2021-03-09 15:17:30 +0800

    Update schedule_task.go

--- common/schedule_task.go
+++ common/schedule_task.go
@@ -26,14 +26,14 @@ type QyTask struct {
 func StartSchedule(tasks []*QyTask, async bool) error {
 	ctx := context.Background()
 	if len(tasks) == 0 {
-		log.Errorf(ctx, "start time task,but no task")
+		//log.Errorf(ctx, "start time task,but no task")
 		return errors.New("no time task")
 	}
 	c := cron.New()
 	for _, t := range tasks {
 		log.Errorf(ctx, "start time task")
 		if err:=c.AddFunc(t.Spec, t.Run); err != nil{
-			log.Errorf(ctx, "start time task,but add task error error is %v",err)
+			//log.Errorf(ctx, "start time task,but add task error error is %v",err)
 		}
 	}
 	if async {
